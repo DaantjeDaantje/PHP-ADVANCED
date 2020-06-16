95f5e98d7fa99cbd40223d7b0259f3f63dcb021b
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-06-16 18:47:56 +0200

    Functions 1

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        function schrijfKleur(){
+            echo "<p style='color:red'>Deze tekst heeft de kleur rood</p>";
+            echo "<p style='color:blue'>Deze tekst heeft de kleur blauw</p>";
+            echo "<p style='color:yellow'>Deze tekst heeft de kleur geel</p>";
+            echo "<p style='color:purple'>Deze tekst heeft de kleur paars</p>";
+        }
+        schrijfKleur();
+	?>
+</body>
+
+</html>

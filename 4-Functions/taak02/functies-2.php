ddb2e94e26a9d1b968bfe9d719bdbecb7e7d7e7c
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-06-16 20:09:35 +0200

    functions 2

---
+++ 4-Functions/taak02/functies-2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        function rekenUit($getal1, $getal2, $getal3){
+            return $getal1 + $getal2 + $getal3;
+        }
+        echo "Als ik de getallen 12, 14 en 10 bij elkaar optel, is de uitkomst " . rekenUit(12, 14, 10) . "</br>"; 
+        echo "Als ik de getallen 69, 420 en 21 bij elkaar optel, is de uitkomst " . rekenUit(69, 420, 21) . "</br>";
+        echo "Als ik de getallen 45, 47 en 11 bij elkaar optel, is de uitkomst " . rekenUit(45, 47, 11) . "</br>";         
+	?>
+</body>
+
+</html>
